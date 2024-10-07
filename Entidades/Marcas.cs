552d4e65ec7b7b8f6ad3fd6c698e2d2960b4bd4e
CarlosDiaz <carlosalbertodiaz023@gmail.com>
2024-10-07 09:07:26 -0600

    Constructor

--- Entidades/Marcas.cs
+++ Entidades/Marcas.cs
@@ -9,6 +9,13 @@ namespace Entidades
 {
     public class Marcas
     {
+        public Marcas(int idMarcas, string nombre, string descripcion)
+        {
+            IdMarcas = idMarcas;
+            Nombre = nombre;
+            Descripcion = descripcion;
+        }
+
         public int IdMarcas { get; set; }
         public string Nombre { get;set; }
         public string Descripcion { get;}
