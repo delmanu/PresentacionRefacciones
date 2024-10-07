a721d218824d20f78f6753867da9a42918582d2c
CarlosDiaz <carlosalbertodiaz023@gmail.com>
2024-10-07 08:45:14 -0600

    Commit 1

--- AccesoDatos/Base.cs
+++ AccesoDatos/Base.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace AccesoDatos
 {
-    public class Class1
+    public class Base
     {
+
     }
 }
--- Entidades/Marcas.cs
+++ Entidades/Marcas.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class Class1
+    public class Marcas
     {
+        //COMMIT DE PRUEBA 1
     }
 }
